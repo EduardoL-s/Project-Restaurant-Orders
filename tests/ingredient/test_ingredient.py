7ad21d8cb53a39c24902b70bd26bcda403a55539
EduardoL-s <eduardoluizmm@hotmail.com>
2023-07-16 04:56:51 -0300

    [Requisito 1] Implementação dos testes referentes a Class Ingredient - Concluído

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("camarão")
+    assert ingredient.name == "camarão"
+    assert ingredient.restrictions == {
+            Restriction.ANIMAL_MEAT,
+            Restriction.SEAFOOD,
+            Restriction.ANIMAL_DERIVED,
+        }
+    assert ingredient.__repr__()  == "Ingredient('camarão')"
+    assert ingredient.__eq__(ingredient) is True
+    assert ingredient.__hash__() == hash(ingredient.name)
+
+
