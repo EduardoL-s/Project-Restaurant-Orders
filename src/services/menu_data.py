43ed321bcb4e519043c0e6868db8bb6e5d7940e4
EduardoL-s <eduardoluizmm@hotmail.com>
2023-07-16 07:12:01 -0300

    [Requisito 3] Mapeamento dos pratos - Concluido

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,25 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        with open(source_path, 'r') as arquivo:
+            arquivo_csv =  list(csv.DictReader(arquivo))
+            dish_exist = {}
+
+            for row in arquivo_csv:
+                dish = dish_exist.setdefault(
+                    row['dish'],
+                    Dish(row['dish'], float(row['price'])))
+
+                dish.add_ingredient_dependency(
+                    Ingredient(row['ingredient']),
+                    int(row['recipe_amount']))
+                self.dishes.add(dish)
+
+            
